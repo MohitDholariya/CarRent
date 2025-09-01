1a386359729d1952df03066dbd20510d05ac45b0
divyamlathiya <divyamlathiya88@gmail.com>
2025-09-01 14:48:56 +0530

    project completed

---
+++ client/src/components/Newsletter.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import { motion } from 'motion/react'
+
+const Newsletter = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+
+    if (!email.trim()) {
+      toast.error('Please enter a valid email address');
+      return;
+    }
+
+    // Optionally, send email to backend here
+
+    toast.success(`${email} subscribed successfully!`);
+    setEmail(''); // clear input field
+  };
+
+  return (
+    <motion.div 
+    initial = {{ opacity : 0, y: 30 }}
+    whileInView = {{ opacity: 1, y: 0 }}
+    transition = {{ duration: 0.6, ease: "easeOut" }}
+    viewport = {{ once: true, amount: 0.3 }}
+
+    className="flex flex-col items-center justify-center text-center space-y-2 max-md:px-4 my-10 mb-40">
+      <motion.h1 
+      initial = {{ opacity : 0, y: 20 }}
+      whileInView = {{ opacity: 1, y: 0 }}
+      transition = {{ duration: 0.5, delay: 0.2 }}
+      className="md:text-4xl text-2xl font-semibold">Never Miss a Deal!
+      </motion.h1>
+
+      <motion.p 
+      initial = {{ opacity : 0, y: 20 }}
+      whileInView = {{ opacity: 1, y: 0 }}
+      transition = {{ duration: 0.5, delay: 0.3 }}
+      className="md:text-lg text-gray-500/70 pb-8">
+        Subscribe to get the latest offers, new arrivals, and exclusive discounts
+      </motion.p>
+
+      <motion.form 
+      initial = {{ opacity : 0, y: 50 }}
+      whileInView = {{ opacity: 1, y: 0 }}
+      transition = {{ duration: 0.5, delay: 0.6 }}
+      onSubmit={handleSubscribe} className="flex items-center justify-between max-w-2xl w-full md:h-13 h-12">
+        <input
+          className="border border-gray-300 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500"
+          type="email"
+          placeholder="Enter your email id"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="md:px-12 px-8 h-full text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer rounded-md rounded-l-none"
+        >
+          Subscribe
+        </button>
+      </motion.form>
+    </motion.div>
+  );
+};
+
+export default Newsletter;
